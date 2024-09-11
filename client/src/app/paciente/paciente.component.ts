2839e7344ff1421d19637e0d6222d319d499b751
Alexis Arath Alatorre Delgadillo <alexisaalatorred@gmail.com>
2024-09-11 15:12:21 -0700

    Vista paciente agregada

--- client/src/app/paciente/paciente.component.ts
+++ client/src/app/paciente/paciente.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class PacienteComponent {
 
+  isMinimized: boolean = false;
+
+  toggleSidebar() {
+    this.isMinimized = !this.isMinimized;
+  }
+  
 }
