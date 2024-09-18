90132ca4b1fdef094ea78f5e4b4d6bf6461406ff
Anthony Lamas <anthony21.lamas@gmail.com>
2024-09-18 10:38:57 -0700

    Responsiva Moviles

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -16,9 +16,15 @@ module.exports = {
         'cancel-color': '',
       },
       screens:{
-        sm: '640px',
-        md: '768px',
-        lg: '1024px',
+        'xxs': '360px',
+        'xs': '480px',
+        'sm': '640px',
+        'md': '768px',
+        'mdx': '990px', 
+        'lg': '1024px',
+        'slg': '1120px',
+        'xl': '1280px',
+        '2xl': '1536px',
       
       }
     },
